90ea8932889927d8af962394cde6f1c96c5fd00e
Guilherme Augusto <guilhermeaugustodeoliveira66@gmail.com>
2022-10-31 11:32:32 -0300

    Visual da tela inicial renovada

--- lib/main.dart
+++ lib/main.dart
@@ -51,63 +51,58 @@ class MobileView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: Column(
-        children: [
-          Image.asset(
-            'images/logo.png',
-            width: 400,
-            alignment: Alignment.topCenter,
-          ),
-          SizedBox(
-            width: width * 0.8,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: const [
-                Text(
-                  'Sistema de Análises Clínicas\n',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontSize: 30,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.blue,
-                  ),
+    return Center(
+      child: SingleChildScrollView(
+        padding: const EdgeInsets.all(12.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Image.asset(
+              'images/logo.png',
+              width: 400,
+            ),
+            const Text(
+              'Sistema de Análises Clínicas\n',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 30,
+                fontWeight: FontWeight.bold,
+                color: Colors.blue,
+              ),
+            ),
+            const Text.rich(
+              overflow: TextOverflow.clip,
+              textAlign: TextAlign.justify,
+              softWrap: true,
+              TextSpan(
+                style: TextStyle(
+                  fontSize: 20,
                 ),
-                Text.rich(
-                  overflow: TextOverflow.clip,
-                  textAlign: TextAlign.justify,
-                  softWrap: true,
+                children: <TextSpan>[
                   TextSpan(
-                    style: TextStyle(
-                      fontSize: 20,
-                    ),
-                    children: <TextSpan>[
-                      TextSpan(
-                        text:
-                            'Bem-vindo ao Treina-Voz, uma ferramenta didática '
-                            'em Fonoaudiologia voltada para o desenvolvimento do raciocínio clínico na área de VOZ.\n\n',
-                      ),
-                      TextSpan(
-                        text:
-                            'Aqui você terá a oportunidade de exercitar autonomamente tanto o seu raciocínio em diagnóstico quanto em conduta terapêutica voltados para pacientes adultos com disfonia comportamental.',
-                      ),
-                    ],
+                    text: 'Bem-vindo ao Treina-Voz, uma ferramenta didática '
+                        'em Fonoaudiologia voltada para o desenvolvimento do raciocínio clínico na área de VOZ.\n\n',
                   ),
-                ),
-              ],
+                  TextSpan(
+                    text:
+                        'Aqui você terá a oportunidade de exercitar autonomamente tanto o seu raciocínio em diagnóstico quanto em conduta terapêutica voltados para pacientes adultos com disfonia comportamental.',
+                  ),
+                ],
+              ),
             ),
-          ),
-          const SizedBox(
-            height: 30,
-          ),
-          SizedBox(
-            width: width * 0.8,
-            child: ElevatedButton(
-              onPressed: () {},
-              child: const Text('Iniciar'),
+            const SizedBox(
+              height: 30,
             ),
-          ),
-        ],
+            SizedBox(
+              width: width * 0.8,
+              child: ElevatedButton(
+                onPressed: () {},
+                child: const Text('Iniciar'),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
